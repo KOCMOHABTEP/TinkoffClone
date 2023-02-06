1376fcda12121e504080fd553bdad000eecac8be
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2023-02-07 00:14:49 +0300

    Stable build

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { AuthProvider } from "./app/providers/AuthProvider";
+import {AuthProvider} from './app/providers/AuthProvider';
+import Navigation from './app/navigation/Navigation';
 
 export default function App() {
-  return {
+  return (
     <AuthProvider>
       <Navigation />
     </AuthProvider>
-  }
+  );
 }
-
