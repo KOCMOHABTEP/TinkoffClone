304e4b2653d47867c8405e738b368a56b3be8557
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2023-02-10 23:41:03 +0300

    Support chat with messages screen

--- app/components/screens/support/Support.tsx
+++ app/components/screens/support/Support.tsx
@@ -1,11 +1,29 @@
 import React, {FC} from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text} from 'react-native';
+import {Layout} from '../../layout/Layout';
+import Padding from '../../ui/Padding';
+import Header from './Header';
+import Message from './Message';
+import {useMessages} from '../../../hooks/useMessages';
+import InputField from './InputField';
 
 const Support: FC = () => {
+  const {messages} = useMessages();
+
   return (
-    <View>
+    <Layout isScrollView={false}>
+      <Padding>
+        <Header />
+        <ScrollView style={{height: '83%'}}>
+          {messages.map(message => (
+            <Message key={message._id} message={message} />
+          ))}
+        </ScrollView>
+        <InputField />
+      </Padding>
+
       <Text>Support</Text>
-    </View>
+    </Layout>
   );
 };
 
--- app/styles.ts
+++ app/styles.ts
@@ -15,11 +15,13 @@ export const UI_GREY_100 = 'rgb(243 244 246)';
 export const UI_GREY_200 = 'rgb(229 231 235)';
 export const UI_GREY_300 = 'rgb(209 213 219)';
 export const UI_GREY_500 = 'rgb(107 114 128)';
+export const UI_GREY_700 = 'rgb(55 65 81)';
 export const UI_GREY_800 = 'rgb(31 41 55)';
 
 export const UI_YELLOW_300 = 'rgb(253 224 71)';
 
 export const UI_GREEN_500 = 'rgb(34 197 94)';
 
+export const UI_BLUE_300 = 'rgb(147 197 253)';
 export const UI_BLUE_400 = 'rgb(96 165 250)';
 export const UI_BLUE_500 = 'rgb(59 130 246)';
